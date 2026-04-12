2c8afd2545412f82a4b887796b8e619ea131e3d7
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-12 14:18:55 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -348,8 +348,10 @@ app.delete('/api/accounts/:id', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
 
